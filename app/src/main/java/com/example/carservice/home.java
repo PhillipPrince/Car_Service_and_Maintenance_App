674b43bdae10ad3ce0704d8c454d635f5db23f23
Philip <phillipsimiyu38@gmail.com>
2021-11-19 14:20:09 +0300

    Design Edited

--- app/src/main/java/com/example/carservice/home.java
+++ app/src/main/java/com/example/carservice/home.java
@@ -1,41 +1,70 @@
 package com.example.carservice;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-public class home extends AppCompatActivity {
+public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar=findViewById(R.id.toolbar);
+
+        navigationView=findViewById(R.id.navigatio);
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolbar);
+
+
         setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.Open_navigation, R.string.close_navigation);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
         Intent intent;
-        if(item.getItemId()==R.id.car){
-            intent=new Intent(getApplicationContext(), car.class);
-            startActivity(intent);
-        }else if(item.getItemId()==R.id.home){
-            intent =new Intent(getApplicationContext(), home.class);
+        if(item.getItemId()==R.id.home){
+            intent=new Intent(getApplicationContext(), home.class);
             startActivity(intent);
         }
-        return super.onOptionsItemSelected(item);
+        else if(item.getItemId()==R.id.Addcar){
+            setContentView(R.layout.addcar);
+        }else if(item.getItemId()==R.id.myService)
+            setContentView(R.layout.myservice);
+
+        return true;
     }
 }
